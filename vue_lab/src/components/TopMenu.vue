818becdadc64c467a31779a233ed2bb431574520
cc <guoliwei1992@qq.com>
2018-09-20 17:15:57 +0800

    update vue_lab

---
+++ vue_lab/src/components/TopMenu.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <div>
+            <a id = "logo" href="/"></a>
+            <ul id="nav"></ul>
+            <div id='app'>
+                {{message}}
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    el:"#app",
+    date:{
+        message:'Hello Vue'
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ vue_lab/src/router/BaseButton/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import BaseButton from '@/components/BaseButton'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'BaseButton',
+    component: BaseButton
+  }]
+})
