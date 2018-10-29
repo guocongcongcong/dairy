b8ced3846af96d50b77230fe2bde80086929da9a
cc <guoliwei1992@qq.com>
2018-10-29 17:28:40 +0800

    update

---
+++ vue_lab/src/components/HelloWorld/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='input'>
+        <a>{{msg}}</a>
+        <input name= 'input1' class = 'input1' />
+    </div>
+</template>
+<script>
+export default {
+  name: 'Button',
+  data () {
+    return {
+      msg: '输入框：'
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- vue_lab/src/router/index.js
+++ vue_lab/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
+  routes: [ {
     path: '/',
     name: 'HelloWorld',
-    component: {
-      'HelloWorld': HelloWorld
-    }
+    component: HelloWorld
   }]
 })
